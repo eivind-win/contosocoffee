9f5fa17ebdbb29973616ba49c9a4362fbf41ddfd
Eivind Win <Eivind.Win@basefarm-orange.com>
2023-03-10 15:35:00 +0100

    added role assignment and access to web app

--- case study/main.tf
+++ case study/main.tf
@@ -1,17 +1,3 @@
-# Define the Azure provider and required version
-
-terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "3.46.0"
-    }
-  }
-}
-
-provider "azurerm" {
-  features {}
-}
 
 # Create the resource group
 resource "azurerm_resource_group" "contosocoffee" {
@@ -37,6 +23,20 @@ resource "azurerm_storage_account" "example" {
 
 }
 
+resource "azurerm_user_assigned_identity" "example" {
+  location            = azurerm_resource_group.contosocoffee.location
+  name                = "contosocoffmi"
+  resource_group_name = azurerm_resource_group.contosocoffee.name
+}
+
+#create a container registry
+
+resource "azurerm_role_assignment" "roleassignmentforidentity" {
+  scope                = azurerm_container_registry.coffeshop.id
+  role_definition_name = "AcrPull"
+  principal_id         = azurerm_user_assigned_identity.example.principal_id
+}
+
 resource "azurerm_container_registry" "coffeshop" {
   name                = "coffeshop"
   location            = "eastus"
@@ -45,4 +45,65 @@ resource "azurerm_container_registry" "coffeshop" {
   admin_enabled       = true
 }
 
+
+resource "azurerm_container_group" "example-london" {
+  name                = "example-container-group-london"
+  location            = "North Europe"
+  resource_group_name = azurerm_resource_group.contosocoffee.name
+  ip_address_type     = "Public"
+  dns_name_label      = "example-dns-london"
+  os_type = "Linux"
+    image_registry_credential {
+   user_assigned_identity_id = azurerm_user_assigned_identity.example.id
+   server = azurerm_container_registry.coffeshop.login_server
+  }
+
+  identity {
+    type = "UserAssigned"
+    identity_ids = [ azurerm_user_assigned_identity.example.id ]
+  }
+  container {
+
+    image  = "${azurerm_container_registry.coffeshop.login_server}/coffee-app:latest"
+    cpu    = "1.0"
+    memory = "1.5"
+    name = "coffe-app"
+     
+    
+    ports {
+      port     = 80
+      protocol = "TCP"
+    }
+  }
+}
+
+resource "azurerm_container_group" "example-new-york" {
+  name                = "example-container-group-new-york"
+  location            = "East US"
+  resource_group_name = azurerm_resource_group.contosocoffee.name
+  ip_address_type     = "Public"
+  dns_name_label      = "example-dns-new-york"
+  os_type = "Linux"
+
+  image_registry_credential {
+   user_assigned_identity_id = azurerm_user_assigned_identity.example.id
+   server = azurerm_container_registry.coffeshop.login_server
+  }
+
+  identity {
+    type = "UserAssigned"
+    identity_ids = [ azurerm_user_assigned_identity.example.id ]
+  }
+  container {
+   
+    image  = "${azurerm_container_registry.coffeshop.login_server}/coffee-app:latest"
+    cpu    = "1.0"
+    memory = "1.5"
+    name = "coffe-app"
+    ports {
+      port     = 80
+      protocol = "TCP"
+    }
+  }
+}
 
---
+++ case study/providers.tf
@@ -0,0 +1,14 @@
+# Define the Azure provider and required version
+
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "3.47.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
